e9ace22ed2858ce3e5a6ac49b63b0c66baa7ea56
Andy <andrew.dunn@ajja.org.uk>
2023-05-31 15:48:50 +0100

    feat(ErrorData): Add custom Serialize impl
    This commit adds a custom implementation of `Serialize for ErrorData`,
    which should be more efficient than the derived version,
    due to not being bound by the restrictions of the Rust version set
    for `serde`.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ extern crate alloc;
 use core::fmt;
 
 use alloc::string::String;
-use serde::Deserialize;
+use serde::{ser::SerializeStruct, Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Deserialize)]
 pub struct ErrorData<Msg = String, ED = serde_json::Value> {
@@ -13,4 +13,48 @@ pub struct ErrorData<Msg = String, ED = serde_json::Value> {
     pub message: Msg,
     pub code: Option<serde_json::Number>,
     pub data: Option<ED>,
+}
+
+// Honestly, the derived impl for `Serialize` is a complete mess for `ErrorData`
+// so I prefered to write my own implementation...
+impl<Msg, ED> Serialize for ErrorData<Msg, ED>
+where
+    Msg: AsRef<str>,
+    ED: Serialize,
+{
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let Self {
+            message,
+            code,
+            data,
+        } = self;
+
+        // By all accounts, casting `bool`s to `usize`s is a wonky approach,
+        // but it's the most concise option in this case...
+        let some_count = code.is_some() as usize + data.is_some() as usize;
+        let mut state = serializer.serialize_struct("ErrorData", 1 + some_count)?;
+
+        state.serialize_field("message", message.as_ref())?;
+
+        match code {
+            // Because we want to skip the `code` field entirely
+            // if its value is `None`, we can avoid the overhead
+            // of serializing the `Option`,
+            // and instead, serialize just the inner value.
+            Some(code) => state.serialize_field("code", code),
+            None => state.skip_field("code"),
+        }?;
+
+        match data {
+            // Similarly to above, we want to skip the `data` field entirely,
+            // if its value is `None`.
+            Some(data) => state.serialize_field("data", data),
+            None => state.skip_field("data"),
+        }?;
+
+        state.end()
+    }
 }
